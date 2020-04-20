f11908ba8d36f462ec7cdd64cd1e16b42afded86
Konstantin Sekret <konstantine.sekret@finastra.com>
2020-04-20 10:49:39 +0300

    initial playground

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs';
+var observable = Observable.create((observer:any) => {
+    observer.next('Hello World!');
+    observer.next('Hello Again!');
+    observer.complete();
+    observer.next('Bye');
+})
+observable.subscribe(
+    (x:any) => logItem(x),
+    (error: any) => logItem ('Error: ' + error),
+    () => logItem('Completed')
+);
+function logItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("list").appendChild(node);
+}
+
+import { timer, of} from 'rxjs';
+import { mapTo, startWith, scan, tap, map, filter, takeLast, takeUntil, takeWhile } from 'rxjs/operators';
+
+//emit 0 after 1 second then complete, since no second argument is supplied
+//const source = timer(1000,1000);
+//output: 0
+//const subscribe = source.pipe(map(_ => _), scan((acc, curr) => acc+curr, 1),  startWith(444)).subscribe(val => console.log(val));
+
+const source = timer(0, 100)
+
+const terminator = timer(1500)
+
+source.pipe(takeUntil(terminator)).subscribe(_ => console.log(_))
