bb58e2173ee31a5207d623f90977f11400678a6f
Konstantin Sekret <konstantine.sekret@finastra.com>
2020-11-06 18:25:46 +0300

    null check

---
+++ src/28-checkStrickNullCheck.ts
@@ -0,0 +1,15 @@
+import { ReplaySubject, interval, timer, Subject, merge, of, Observable, Subscription, throwError } from "rxjs"
+import { take, takeUntil, scan, map, tap, shareReplay, switchMap, delay, finalize, observeOn, share, filter, startWith, timeoutWith, catchError } from "rxjs/operators"
+import { TestScheduler } from 'rxjs/internal/testing/TestScheduler';
+import { asap } from "rxjs/internal/scheduler/asap";
+import { queue } from "rxjs/internal/scheduler/queue";
+import { async } from "rxjs/internal/scheduler/async";
+import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
+
+const startTime = Date.now()
+
+const consoleHandler = (prefix: any) => (value: any) => console.log(`At ${Date.now() - startTime}: ${prefix} ${JSON.stringify(value)}`)
+
+export function checkStrickNullCheck(): string | undefined {
+    return undefined;
+}
