c9ba1fc8d8f378c365fbb7c491a0e38f55baa3d6
sekretk <boyko.constantine@gmail.com>
2021-01-21 22:14:41 +0300

    types test

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,3 +10,39 @@ const result = config
   .option('name', 'type-challenges')
   .option('bar', { value: 'Hello World' })
   .get()
+
+  type SingleAssignmentChainable<T = {}> = {
+    option<V>(key: Exclude<string, keyof T>, value: V): SingleAssignmentChainable<T & Record<typeof key, V>>;
+    get(): T;
+  };
+  
+  type LastAssignmentChainable<T = {}> = {
+    option<K extends string, V>(key: K, value: V): LastAssignmentChainable<Omit<T, K> & Record<K, V>>;
+    get(): T;
+  };
+  
+  type FirstAssigmentChainable<T = {}> = {
+    option<K extends string, V>(key: K, value: V): FirstAssigmentChainable<K extends keyof T ? T : Omit<T, K> & Record<K, V>>;
+    get(): T;
+  };
+  
+  declare const l: LastAssignmentChainable
+  declare const s: SingleAssignmentChainable
+  declare const f: FirstAssigmentChainable
+  
+  // LastAssignmentChainable
+  const lastAssigmentResult = l
+    .option('foo', '123')
+    .option('foo', 123) // this is the value and type of `foo` we want to keep
+    .get()
+  
+  // FirstAssigmentChainable
+  const firstAssignmentResult = f
+    .option('foo', '123') // this is the value and type of `foo` we want to keep
+    .option('foo', 123)
+    .get()
+  
+  // SingleAssignmentChainable
+  s.option('foo', '123')
+   .option('foo', 123) // this error is expected, yay only one assigment of `foo`
+   .get()
