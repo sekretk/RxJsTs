5a009246ebc638512a5b09d948adc2205bbf5a0c
sekretk <boyko.constantine@gmail.com>
2021-03-26 20:26:24 +0300

    some type

---
+++ src/types/sandbox.ts
@@ -0,0 +1,18 @@
+type Union<T extends any[]> = T extends [infer L, ...infer R] ? L & Union<R> : {};
+type FunctionResults<T extends any[]> = T extends [FuncResult<infer L>, ...infer R] ? [L, ...FunctionResults<R>] : [];
+
+type FuncResult<T> = (a: any) => T;
+
+const mainFunc = <T extends any[]>(args: T): Union<T> => 
+    args.reduce((acc, val) => ({...acc, ...val}), {})
+
+const fabrics = [() => ({a: 1}), () => ({b: 'x'})];
+
+type params = [{a: 1}, {b: 'x'}];
+
+const rr: TupleToUnion<params>;
+
+rr.toExponential
+
+type TupleToUnion<T extends Array<any>> = T[number];
+
