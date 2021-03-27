3eb6f6fe57c747f886ee537a72b9eb78def09837
sekretk <boyko.constantine@gmail.com>
2021-03-27 11:49:18 +0300

    create type

--- src/types/argsAsTuple.ts
+++ src/types/argsAsTuple.ts
@@ -1,8 +1,18 @@
+import { combineLatest, merge, Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
+
 type First<T extends any[]> = T extends [infer F, ... infer R] ? F : T;
 
 type DropFirst<T extends any[]> = T extends [infer F, ... infer R] ? R : T;
 
 type UnionAll<T extends any[]> = T extends [infer F, ...infer R] ? F & UnionAll<R> : T;
+
+type DistinctiveUnion<T extends any[]> =
+    T extends [infer F, ...infer R]
+    ? keyof F extends Exclude<keyof F, keyof DistinctiveUnion<R>>
+    ? F & DistinctiveUnion<R>
+    : {}
+    : T;
 
 type TSubject01 = [number, boolean, string, Array<string>];
 
@@ -18,38 +28,22 @@ type EX01 = LastInTuple<TSubject01>;
 
 type TSubject02 = [{ a: string }, { b: number }, { c: boolean }];
 
-const wrappFunc = <T extends any[]>(...args: T): UnionAll<T> => args as UnionAll<T>;
-
-const restrictedFunc = (arg: {a: number, b: string, c: boolean}):void => {};
+const restrictedFunc = (arg: { a: number, b: string, c: boolean }): void => { };
+const resultGen = <T extends any[]>(...args: T): DistinctiveUnion<T> => args as DistinctiveUnion<T>;
 
 restrictedFunc(
-    wrappFunc(
-        {a: 1},
-        {b: '1', c: true},
-        {c: false},
+    resultGen(
+        { a: 1 },
+        { b: '1', d: true },
+        { c: false },
     )
 )
 
-type KeysIntersect<T, R> = keyof T & keyof R;
-
-type AllKeyIntersect<T extends any[]> = T extends [infer F, ... infer R] 
-            ? KeysIntersect<F, AllKeyIntersect<R>> 
-            : never;
-
-type TSubject025 = [{ r: string }, { b: number }, { c: boolean, b: number }];
-type EX25 = AllKeyIntersect<TSubject025>;
-
-const a = {a: 1};
-const b = {b: '1', c: '2'};
-
-type INTR_EX01 = KeysIntersect<typeof a, typeof b>;
-
-type DistinctedUnion<T extends any[]> = AllKeyIntersect<T> extends never ? UnionAll<T> : never;
-
-type EX03 = DistinctedUnion<TSubject02>;
-
-type TSubject03 = [{ r: string }, { b: number }, { c: boolean, b: number }];
-
-type EX04 = AllKeyIntersect<TSubject03>;
-
-type EX05 = DistinctedUnion<TSubject03>;
+const stateObs = (): Observable<{ a: number, b: string, c: boolean }> =>
+    combineLatest([
+        of({ a: 1 }),
+        of({ b: '1', d: true }),
+        of({ c: false }),
+    ]).pipe(
+        map(x => resultGen(...x)),
+    );
